4a6108180204c6b12b351d808f1367faafe2448a
LEBREUILLY Fabien <FLEBREUILLY@pedago.lan>
2019-01-07 17:02:57 +0100

    07/01

--- cpartie/page-connexion.php
+++ cpartie/page-connexion.php
@@ -3,12 +3,14 @@ get_header();
 ?>
 <div class="cp-cgu row">
 	<div class="col-md-4 cp-carre">
-		E-mail/Pseudo<br>
-		<input type="text"><br>
-		Password<br>
-		<input type="password">
-		<button class="bouton-connexion">Se connecter</button><a href="<?php bloginfo('url')?>/inscription"><button class="bouton-connexion float">Rejoins-nous !</button></a>
-		<div class="centre"><a href="<?php bloginfo('url')?>/mdpoublie">Mot de passe perdu ?</a></div>
+		<form action="" method="post">
+			E-mail/Pseudo<br>
+			<input name="mail" type="text"><br>
+			Password<br>
+			<input name="password" type="password">
+			<button name="action" class="bouton-connexion">Se connecter</button><a href="<?php bloginfo('url')?>/inscription"><button class="bouton-connexion float">Rejoins-nous !</button></a>
+			<div class="centre"><a href="<?php bloginfo('url')?>/mdpoublie">Mot de passe perdu ?</a></div>
+		</form>
 	</div>
 </div>
 <?php get_footer(); 
--- cpartie/page-inscription.php
+++ cpartie/page-inscription.php
@@ -1,25 +1,41 @@
 <?php 
 get_header(); 
+global $wpdb;
 ?>
 <div class="cp-cgu row">
 	<div class="col-md-12 col-lg-9 col-xl-5 cp-carre2 cp-inscription">
-		<div class="row">
-			<div class="col-6">
-				E-mail<br>
-				<input type="text"><br>
-				Pseudo<br>
-				<input type="text"><br>
+		<form method="POST" action="">
+			<div class="row">
+				<div class="col-6">
+					E-mail<br>
+					<input name="mail" type="text" required><br>
+					Pseudo<br>
+					<input name="pseudo" type="text" required><br>
+				</div>
+				<div class="col-6">
+					Password<br>
+					<input name="password" type="password" required><br>
+					Code postal<br>
+					<input name="cp" type="number" required><br>
+				</div>
 			</div>
-			<div class="col-6">
-				Password<br>
-				<input type="password"><br>
-				Code postal<br>
-				<input type="number"><br>
-			</div>
-		</div>
-		<input type="checkbox"><span class="cgu"><a href="<?php bloginfo('url')?>/cgu" target=_blank>Accepter les CGU</a></span> 
-		<button class="bouton-connexion float">S'inscrire</button>
+			<input type="checkbox" required><span class="cgu"><a href="<?php bloginfo('url')?>/cgu" target=_blank>Accepter les CGU</a></span> 
+			<button type="submit" name="inscription" value="inscrire" class="bouton-connexion float">S'inscrire</button>
+		</form>
 	</div>
 </div>
-<?php get_footer(); 
+<?php
+$ins=$_POST;
+$wpdb->insert('wp_users', array(
+	'ID'=>'',
+    'user_login' => $_POST['pseudo'],
+    'user_pass' => $_POST['password'],
+    'user_nicename' => $_POST['pseudo'],
+    'user_email' => $_POST['mail'],
+	'display_name' => $_POST['pseudo'],
+	'user_cp'=>$_POST['cp'],
+));
+?>
+<?php 
+get_footer(); 
 ?>
